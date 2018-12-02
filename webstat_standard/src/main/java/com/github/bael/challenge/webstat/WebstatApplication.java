1d937bb4cd6516a1244f85d6b65883e6cf478d59
Denis <denkern@gmail.com>
2018-12-02 18:03:16 +0700

    standard variant

---
+++ webstat_standard/src/main/java/com/github/bael/challenge/webstat/WebstatApplication.java
@@ -0,0 +1,12 @@
+package com.github.bael.challenge.webstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstatApplication.class, args);
+	}
+}
---
+++ webstat_standard/src/main/java/com/github/bael/challenge/webstat/data/SiteEntryRepository.java
@@ -0,0 +1,7 @@
+package com.github.bael.challenge.webstat.data;
+
+import com.github.bael.challenge.webstat.entities.SiteEntry;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SiteEntryRepository extends CrudRepository<SiteEntry, Long> {
+}
